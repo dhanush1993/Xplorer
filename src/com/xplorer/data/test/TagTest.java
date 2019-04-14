557d9b6a3aa4b81001cb8d2775093be7242784eb
dhanush1993 <dhanush@github.com>
2019-04-14 02:44:16 -0700

    Test cases for the data models.

---
+++ src/com/xplorer/data/test/TagTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.xplorer.data.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author Dhanush
+ *
+ */
+public class TagTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/com/xplorer/data/test/XFileTest.java
@@ -0,0 +1,236 @@
+/**
+ * 
+ */
+package com.xplorer.data.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author Dhanush
+ *
+ */
+public class XFileTest {
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#XFile(java.lang.String)}.
+	 */
+	@Test
+	public void testXFileString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#XFile(java.lang.String, java.net.URI)}.
+	 */
+	@Test
+	public void testXFileStringURI() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#XFile(java.lang.String, java.net.URI, java.lang.String, java.lang.String)}.
+	 */
+	@Test
+	public void testXFileStringURIStringString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#getFilename()}.
+	 */
+	@Test
+	public void testGetFilename() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#setFilename(java.lang.String)}.
+	 */
+	@Test
+	public void testSetFilename() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#getPath()}.
+	 */
+	@Test
+	public void testGetPath() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#setPath(java.net.URI)}.
+	 */
+	@Test
+	public void testSetPath() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#getFileType()}.
+	 */
+	@Test
+	public void testGetFileType() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#setFileType(java.lang.String)}.
+	 */
+	@Test
+	public void testSetFileType() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#getExt()}.
+	 */
+	@Test
+	public void testGetExt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#setExt(java.lang.String)}.
+	 */
+	@Test
+	public void testSetExt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#getTags()}.
+	 */
+	@Test
+	public void testGetTags() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#setTags(java.util.ArrayList)}.
+	 */
+	@Test
+	public void testSetTags() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#setTag(com.xplorer.data.Tag)}.
+	 */
+	@Test
+	public void testSetTag() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.xplorer.data.XFile#getTag(int)}.
+	 */
+	@Test
+	public void testGetTag() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#Object()}.
+	 */
+	@Test
+	public void testObject() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#getClass()}.
+	 */
+	@Test
+	public void testGetClass() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#hashCode()}.
+	 */
+	@Test
+	public void testHashCode() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#equals(java.lang.Object)}.
+	 */
+	@Test
+	public void testEquals() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#clone()}.
+	 */
+	@Test
+	public void testClone() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public void testToString() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notify()}.
+	 */
+	@Test
+	public void testNotify() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#notifyAll()}.
+	 */
+	@Test
+	public void testNotifyAll() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long)}.
+	 */
+	@Test
+	public void testWaitLong() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait(long, int)}.
+	 */
+	@Test
+	public void testWaitLongInt() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#wait()}.
+	 */
+	@Test
+	public void testWait() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#finalize()}.
+	 */
+	@Test
+	public void testFinalize() {
+		fail("Not yet implemented");
+	}
+
+}
